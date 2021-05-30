ac17390c2b0cd29116479a732db2322d41437b52
matsuokashuhei <matsuokashuheiii@gmail.com>
2021-05-30 07:09:50 +0000

    express

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { ApolloServer } = require(`apollo-server`)
+const { ApolloServer } = require('apollo-server-express')
+const expressPlayground = require('graphql-playground-middleware-express').default
+const express = require('express')
 const { GraphQLScalarType } = require('graphql')
 
 const typeDefs = `
@@ -115,6 +117,10 @@ const resolvers = {
     })
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+var app = express()
+app.get('/', (req, res) => res.end(`Welcome to the PhotoShare API`))
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+app.listen({ port: 4000 }, () => console.log(`GraphQL server running @ http://localhost:4000${server.graphqlPath}`))
 
-server.listen().then(({ url }) => console.log(`GraphQL Service running on ${url}`))
+const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app })
